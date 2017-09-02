a2f00677fe7155ff300fb079d41d7d26ff276ae7
john vascsinec <pensteeler@gmail.com>
2017-09-02 21:24:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+function fatBastard(meal) {
+
+  function whatsForDinner() { // whatsForDinner() is the inner function, a closure
+    if (!meal) { // whatsForDinner() uses argument provided to the parent function
+      console.log('My belly is empty. Woe is me.');
+    } else if (meal === 'Mini-Me') { // whatsForDinner() uses argument provided to the parent function
+      console.log('The wee man is in my belly!');
+    } else {
+      console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+
+  function digest() { // digest() is an inner function, a closure
+    meal = undefined; // digest() uses argument provided to the parent function
+  }
+
+  return  {
+      whatsForDinner;
+      digest
+    };
+
+}
+
+const whatsForDinner = fatBastard('Kobe beef');
+whatsForDinner(); // prints 'I'm eatin' a bit of Kobe beef! Burp.'
+
+// Same as below
+const whatsInHisTummy = fatBastard('Mini-Me');
+whatsInHisTummy(); // prints 'The wee man is in my belly!'
+
+const { whatsForDinner, digest } = fatBastard('ribeye');
+whatsForDinner(); // prints 'I'm eatin' a bit of ribeye! Burp.'
+digest();
+whatsForDinner(); // prints 'My belly is empty. Woe is me.'
